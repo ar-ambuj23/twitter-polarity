9b656be433ff926e16a792369ac84886058e7b25
Skanda Suresh <skoct13@gmail.com>
2020-11-21 08:40:25 -0700

    Adding URL args parsing and jsonify response

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
 from sentiment import polarityByCount, polarityByDay
 
 import json
@@ -6,21 +6,32 @@ import datetime
 
 app = Flask(__name__)
 
+def process_request(args):
+    search_params = dict()
+    search_params['hashtag'] = '#' + args['hashtag']
+    search_params['count'] = int(args['count'])
+    search_params['loc'] = args['loc']
+    return search_params
+
 @app.route('/polarityByCount', methods = ['GET','POST'])
 def getPolarityByCount():
-    hashtag = '#trump'
-    count = 100
-    loc = 'USA'
-    result = polarityByCount(hashtag, count, loc)
-    return Response(json.dumps(result, default=default),  mimetype='application/json')
+    # print("In request: ByCount")
+    args = request.args
+    search_params = process_request(args)
+    # print(search_params)
+    result = polarityByCount(**search_params)
+    return jsonify(result)
+    #return Response(json.dumps(result, default=default),  mimetype='application/json')
 
 @app.route('/polarityByDay', methods = ['GET','POST'])
 def getPolarityByDay():
-    hashtag = '#trump'
-    count = 100
-    loc = 'USA'
-    result = polarityByDay(hashtag, count, loc)
-    return Response(json.dumps(result, default=default),  mimetype='application/json')
+    # print("In request: ByDay")
+    args = request.args
+    search_params = process_request(args)
+    # print(search_params)
+    result = polarityByDay(**search_params)
+    return jsonify(result)
+    #return Response(json.dumps(result, default=default),  mimetype='application/json')
 
 def default(o):
     if isinstance(o, datetime.datetime):
