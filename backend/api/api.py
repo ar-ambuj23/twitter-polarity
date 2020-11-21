456ff4ced91b225acfdb571902ea4763465111b6
ar-ambuj23 <ar.ambuj23@gmail.com>
2020-11-20 19:38:29 -0700

    api code converted to class

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,15 +1,20 @@
 import json
 import tweepy
+import os
 
-credentials_file = 'credentials/twitter_credentials.json'
+class TwitterAPI():
+    def __init__(self, credentials_file_path = os.path.join(os.getcwd(),'credentials/twitter_credentials.json')):
+        
+        credentials = self.get_credentials(credentials_file_path)
+        auth = tweepy.OAuthHandler(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
+        auth.set_access_token(credentials['ACCESS_TOKEN'], credentials['ACCESS_TOKEN_SECRET'])
+        self.api = tweepy.API(auth)
 
-with open(credentials_file) as json_file:
-    credentials = json.load(json_file)
+    def get_credentials(self, credentials_file_path):
+        with open(credentials_file_path) as json_file:
+            credentials = json.load(json_file)
+        return credentials
 
-auth = tweepy.OAuthHandler(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
-auth.set_access_token(credentials['ACCESS_TOKEN'], credentials['ACCESS_TOKEN_SECRET'])
-api = tweepy.API(auth, tweet_mode='extended')    
-
-def searchByHashtag(hashtag, count, lang='en', geocode=None, result_type='mixed'):
-    tweets = api.search('#{}'.format(hashtag), count=count, lang=lang, geocode=geocode, result_type=result_type, tweet_mode='extended')
-    return [(tweet.full_text, tweet.created_at) for tweet in tweets]
+    def searchByHashtag(self, hashtag, count, lang='en', geocode=None, result_type='mixed'):
+        tweets = self.api.search('#{}'.format(hashtag), count=count, lang=lang, geocode=geocode, result_type=result_type, tweet_mode='extended')
+        return [{'full_text':tweet.full_text, 'created_at':tweet.created_at} for tweet in tweets]
