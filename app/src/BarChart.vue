f94e1288fc12309056b20c67bf3d77724e657ef8
nitish <nitishbahl24@gmail.com>
2020-11-22 11:50:32 +0530

    Add methods for parsing graph

--- app/src/BarChart.vue
+++ app/src/BarChart.vue
@@ -1,7 +1,7 @@
 <script>
-import {Line} from 'vue-chartjs'
+import {Bar} from 'vue-chartjs'
 export default {
-    extends: Line,
+    extends: Bar,
     props: {
         chartdata:{
             type: Object,
